584dd42eaf4016a27b1f10b0ee1dd29b69ed3ad2
Claudia Oliveira <capeni.oliveira@gmail.com>
2021-01-08 22:19:35 -0300

    aulaJS

---
+++ js/main.js
@@ -0,0 +1,128 @@
+function botão (){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigada lazarento<b>";
+    //console.log(document.getElementById("agradecimento"))
+    //alert("Valeuuu!");
+}
+
+function redirecionar(){
+    window.open("https://www.gft.com/br/pt/index/");
+    //window.location.href = "https://www.gft.com/br/pt/index/";
+}
+
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Tu é massa viu";
+    elemento.innerHTML = "Tu é massa viu";
+    //alert("trocar o texto");
+}
+
+function voltar(elemento){
+   // document.getElementById("mousemove").innerHTML = "Passe o mouse aqui cumpadi";
+   elemento.innerHTML = "Passe o mouse aqui cumpadi";
+}
+
+function load(){
+    alert("página carregada");
+}
+
+function funcaoChange(elemento){
+console.log(elemento.value);
+}
+
+
+
+
+/*
+function soma(n1, n2){
+    return n1 + n2;
+}
+*/
+/*
+var validar;
+function validaIdade(idade){
+  validar;
+ if (idade >= 18){
+     validar = true
+ }else{
+     validar = false
+ }
+ return validar;
+}
+
+var idade = prompt("Danadinho! Qual é sua idade?");
+validaIdade(idade)
+console.log(validar);
+
+/*
+
+
+//alert (soma(5, 10));
+
+
+/*
+var d = new Date();
+alert(d.getDay());
+alert(d.getHours());
+alert(d.getMinutes())
+*/
+
+
+/*
+var count;
+for(count=0; count <=5; count++){
+    alert(count);
+};
+*/
+
+
+
+/*
+var count = 0;
+while (count <= 5){
+    console.log(count);
+    alert(count);
+    count ++;
+}
+*/
+
+
+
+/*
+var idade = prompt("Qual é sua idade?")
+if (idade >= 18){
+    alert("maior de idade");
+    }else{
+        alert("menor de idade");
+    };
+*/
+
+
+/*
+var frutas = [{nome:"maça", cor:"verde"}, {nome:"uva", cor: "verde"}]
+console.log(frutas);
+alert(frutas[1].nome);
+*/
+
+
+
+/*
+var fruta = ["pera", "uva","maça"]
+lista.pop("salada mista");
+console.log(lista);
+*/
+
+
+//console.log(lista);
+//console.log(lista.toString());
+//console.log(lista.join(" | "));
+
+
+//var nome = "Claudia";
+//var n1 = 34;
+//var n2 = 10;
+//var frase = "Santos é o melhor time do mundo"
+//alert(nome + " tem " + idade +  " anos " );
+//alert(idade + idade2);
+//console.log(nome);
+//console.log(n1 * n2);
+//console.log(frase.toLowerCase());
+//alert(frase.replace("Santos", "Patriots"));
